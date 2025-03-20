90650070476747cdac4cc36e9578c1723106d80a
qq <3563473926@qq.com>
2025-03-20 14:57:10 +0800

    预估订单金额

--- service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/FeeRuleServiceImpl.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/FeeRuleServiceImpl.java
@@ -1,15 +1,57 @@
 package com.atguigu.daijia.rules.service.impl;
 
+import com.atguigu.daijia.model.form.rules.FeeRuleRequest;
+import com.atguigu.daijia.model.form.rules.FeeRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.FeeRuleResponse;
+import com.atguigu.daijia.model.vo.rules.FeeRuleResponseVo;
 import com.atguigu.daijia.rules.mapper.FeeRuleMapper;
 import com.atguigu.daijia.rules.service.FeeRuleService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class FeeRuleServiceImpl implements FeeRuleService {
 
+    private final KieContainer kieContainer;
 
+
+    //计算订单费用
+    @Override
+    public FeeRuleResponseVo calculateOrderFee(FeeRuleRequestForm calculateOrderFee) {
+
+        //封装输入对象
+        FeeRuleRequest feeRuleRequest = new FeeRuleRequest();
+        feeRuleRequest.setDistance(calculateOrderFee.getDistance());
+        feeRuleRequest.setWaitMinute(feeRuleRequest.getWaitMinute());
+        feeRuleRequest.setStartTime(feeRuleRequest.getStartTime());
+        //Drools使用
+        //开启会话
+        KieSession kieSession = kieContainer.newKieSession();
+
+        //封装返回对象
+        FeeRuleResponse feeRuleResponse = new FeeRuleResponse();
+        kieSession.setGlobal("feeRuleResponse",feeRuleResponse);
+        kieSession.insert(feeRuleRequest);
+
+        //出发规则
+        kieSession.fireAllRules();
+
+        //终止会话
+        kieSession.dispose();
+
+        //封装到vo
+        FeeRuleResponseVo vo = new FeeRuleResponseVo();
+        BeanUtils.copyProperties(feeRuleResponse,vo);
+
+        return vo;
+    }
 }
